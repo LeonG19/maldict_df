ebaa9971eb7ff29228159084fd4bf8fe900becfe
LeonG19 <lgarza3@miners.utep.edu>
2025-03-01 11:51:33 -0700

    file to experiment with julia

---
+++ avast_test.jl
@@ -0,0 +1,12 @@
+
+using Pkg
+Pkg.activate("./")
+
+using Flux, MLDataPattern, Mill, JsonGrinder, JSON, Statistics, IterTools, StatsBase, ThreadTools
+using JsonGrinder: suggestextractor, ExtractDict
+using Mill: reflectinmodel
+using CSV, DataFrames
+using Random
+using Dates
+using Plots
+using Printf
